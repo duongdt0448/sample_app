a17b3881ad9f779a88776480376a451ca7f498d6
duongdt0448 <do.thuy.duong@sun-asterisk.com>
2024-07-24 18:21:51 +0700

    Chapter_7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  USER_PARAMS = %I[name email password password_confirmation].freeze
   VALID_EMAIL_REGEX = Regexp.new(Settings.user.email.regex_valid)
   before_save :downcase_email
 
