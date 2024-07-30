394b42aed177bbd3d668957ed783edc69a557d0c
duongdt0448 <do.thuy.duong@sun-asterisk.com>
2024-07-30 16:02:07 +0700

    Chapter_12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   USER_PARAMS = %I[name email password password_confirmation].freeze
   VALID_EMAIL_REGEX = Regexp.new(Settings.user.email.regex_valid)
+  RESET_PASSWORD_PARAMS = %i(password password_confirmation).freeze
 
   validates :name, presence: true,
             length: {maximum: Settings.user.name.max_length}
@@ -9,7 +10,7 @@ class User < ApplicationRecord
             format: {with: VALID_EMAIL_REGEX},
             uniqueness: {case_sensitive: false}
   has_secure_password
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
   before_create :create_activation_digest
@@ -51,6 +52,20 @@ class User < ApplicationRecord
     return false unless digest
 
     BCrypt::Password.new(digest).is_password? token
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.password_reset_expire.hours.ago
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns reset_digest: User.digest(reset_token),
+                   reset_sent_at: Time.zone.now
   end
 
   private
